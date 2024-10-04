25e4c02b4845b5da6beeb45139b65f00c400a0aa
mrsajadpp <mrsajadpp@gmail.com>
2024-10-04 10:31:08 +0530

    testing

--- route/user.js
+++ route/user.js
@@ -1,24 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const moment = require('moment');
-const geoip = require('geoip-lite');
-const mongoose = require('mongoose');
 
-// Utils
-const validator = require("../util/validate");
-const unique = require("../util/unique");
-const { sendMail } = require("../util/email");
-const date = require("../util/date");
-const format = require("../util/format");
-
-// Middleware
-const auth = require("../middleware/auth");
 
 // Database Models Importing
 const Article = require("../model/article/model");
-const ArticleBin = require("../model/article/bin");
 
 // Home page route
 router.get("/", (req, res, next) => {
